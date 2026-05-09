c86d32b61ede62b2a96c0933730513d9d322e430
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2026-05-08 21:40:17 -0300

    Comunicação entre microservices + Serviço de registro e descoberta

--- microservices/product-service/src/main/java/br/edu/atitus/productservice/controllers/ProductController.java
+++ microservices/product-service/src/main/java/br/edu/atitus/productservice/controllers/ProductController.java
@@ -1,5 +1,7 @@
 package br.edu.atitus.productservice.controllers;
 
+import br.edu.atitus.productservice.clients.CurrencyClient;
+import br.edu.atitus.productservice.clients.CurrencyResponse;
 import br.edu.atitus.productservice.dtos.ProductDTO;
 import br.edu.atitus.productservice.entities.ProductEntity;
 import br.edu.atitus.productservice.repositories.ProductRepository;
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
 
     private final ProductRepository repository;
+    private final CurrencyClient currencyClient;
 
-    public ProductController(ProductRepository repository) {
+    public ProductController(ProductRepository repository, CurrencyClient currencyClient) {
         this.repository = repository;
+        this.currencyClient = currencyClient;
     }
 
     @Value("${server.port}")
@@ -32,6 +36,14 @@ public class ProductController {
         Double convertedPrice = null;
         String environment = "Product-service running on port: " + port;
         String requestCurrency = targetCurrency;
+
+        if (targetCurrency.equals(entity.getCurrency())) {
+            convertedPrice = entity.getPrice();
+        } else {
+            CurrencyResponse currency = currencyClient.getCurrency(entity.getCurrency(), targetCurrency);
+            convertedPrice = entity.getPrice() * currency.conversionRate();
+            environment = environment + " - " + currency.environment();
+        }
 
         ProductDTO dto = new ProductDTO(
                 entity.getId(),
